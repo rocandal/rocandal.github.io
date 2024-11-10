b166ef0eece782e3c4eb3a7a7f5ac9cde674c04e
드림오구 <121333344+dreamogu@users.noreply.github.com>
2024-11-11 02:05:00 +0900

    비주얼 애니메이션 추가

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Visual = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsActive(true);
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <section className='visual'>
+    <section className={`visual ${isActive ? 'on' : ''}`}>
       <img
         src='img/background.png'
         alt='배경'
       />
-      <div className='visual_logo on'>
+      <div className='visual_logo'>
         <img
           src='img/visual.png'
           alt='로제라, 황금빛의 서막'
