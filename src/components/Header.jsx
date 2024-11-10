60651d367b38b371f3aaad1a451035844b173ec8
Shoupeach <shoutrawberry@gmail.com>
2024-11-11 01:19:23 +0900

    헤더에 음악 플레이어 삽입

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import MusicPlayer from './MusicPlayer';
 
 const Header = () => {
   return (
     <header>
-      <div className='inner'>
+      <div className="inner">
         <h1>
-          <img
-            src='./img/logo.png'
-            alt='황금의 유산과 세기말 마인크래프트'
-          />
+          <img src="./img/logo.png" alt="황금의 유산과 세기말 마인크래프트" />
         </h1>
-        <div className='header_right'>
+        <div className="header_right">
           <nav>카운트 다운 전체보기</nav>
-          <div>음악 플레이어 자리</div>
+          <MusicPlayer />
         </div>
       </div>
     </header>
