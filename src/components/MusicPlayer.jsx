d0b327a12df57d6e882d03f81e84762d59d3526c
Shoupeach <shoutrawberry@gmail.com>
2024-11-11 01:18:43 +0900

    음악 플레이어 기본 골조

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,18 @@
-const MusicPlayer = () => {};
+const MusicPlayer = () => {
+  return (
+    <div className="music_player">
+      <div className="music_button_container">
+        <button>&lt;</button>
+        <button>▶️</button>
+        <button>⏸︎</button>
+        <button>&gt;</button>
+      </div>
+      <div className="music_inform_container">
+        <span> · </span>
+        <span>Spoken Without End</span>
+      </div>
+    </div>
+  );
+};
 
 export default MusicPlayer;
