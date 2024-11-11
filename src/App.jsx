7155193780a17cbf2025afa11c29faa6372d4505
Shoupeach <shoutrawberry@gmail.com>
2024-11-12 01:58:34 +0900

    메인 페이지 라우터 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import './style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -5,9 +6,12 @@ import Main from './components/Main';
 
 const App = () => {
   return (
-    <div classNameName='App'>
+    <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        {/* <Route path="/경로명" elemnet={<컴포넌트 />} /> */}
+        <Route path="/" element={<Main />} />
+      </Routes>
       <Footer />
     </div>
   );
