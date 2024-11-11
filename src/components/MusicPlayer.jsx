b28e0ae771f4356c6ee839805e91c47db88ce4fe
Shoupeach <shoutrawberry@gmail.com>
2024-11-12 00:52:31 +0900

    음악 플레이어 기능 기본 골조

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+
 const MusicPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTrack, setCurrentTrack] = useState(0);
+
+  const tracks = ['Song1', 'Song2'];
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+  const handlePrevious = () => {
+    setCurrentTrack((curTrack) =>
+      !curTrack ? tracks.length - 1 : curTrack - 1
+    );
+    setIsPlaying(true);
+  };
+  const handleNext = () => {
+    setCurrentTrack((curTrack) => (curTrack + 1) % tracks.length);
+    setIsPlaying(true);
+  };
+
   return (
     <div className="music_player">
       <div className="music_button_container">
-        <button>&lt;</button>
-        <button>▶️</button>
-        <button>⏸︎</button>
-        <button>&gt;</button>
+        <button onClick={handlePrevious}>&lt;</button>
+        <button onClick={handlePlayPause}>{isPlaying ? '⏸︎' : '▶️'}</button>
+        <button onClick={handleNext}>&gt;</button>
       </div>
       <div className="music_inform_container">
         <span> · </span>
-        <span>Spoken Without End</span>
+        <span>{currentTrack}</span>
       </div>
     </div>
   );
