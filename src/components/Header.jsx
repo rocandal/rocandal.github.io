09b6c14d7ca3b87adba3f7df23e47ce9d018814a
드림오구 <121333344+dreamogu@users.noreply.github.com>
2024-11-15 19:43:22 +0900

    로고 링크 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,14 @@ const Header = () => {
     <header>
       <div className='inner inner_header'>
         <h1>
-          <img
-            src='./img/logo.png'
-            alt='황금의 유산과 세기말 마인크래프트'
-          />
+          <Link to='/'>
+            <img
+              src='./img/logo.png'
+              alt='황금의 유산과 세기말 마인크래프트'
+            />
+          </Link>
         </h1>
+
         <div className='header_right'>
           <nav>
             <Link to='/total'>카운트 다운 전체보기</Link>
