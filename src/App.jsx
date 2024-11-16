80182f349bf73a39c863f079a4aa1bf80e387b79
Shoupeach <shoutrawberry@gmail.com>
2024-11-16 15:17:06 +0900

    404 리다이렉트 다른 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import TotalCount from './components/TotalCount';
+import { useEffect } from 'react';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectPath = urlParams.get('redirect');
+
+    if (redirectPath) navigate(redirectPath, { replace: true });
+  }, [navigate]);
+  
   return (
     <div className="App">
       <Header />
