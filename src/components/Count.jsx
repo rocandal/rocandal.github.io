9c231ab06b99241201e1b3a778a33b993b7ad390
Shoupeach <shoutrawberry@gmail.com>
2024-11-16 15:56:35 +0900

    순서 및 이미지 변경

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -25,16 +25,14 @@ const Count = ({ countItems }) => {
   }, []);
 
   return (
-    <main>
-      <section className="count inner">
-        <h2 className="blind">오늘의 카운트다운</h2>
-        <ul className="count_list">
-          {todayCountItems.map((item, index) => (
-            <CountItem key={index} countData={item} />
-          ))}
-        </ul>
-      </section>
-    </main>
+    <section className="count">
+      <h2 className="blind">오늘의 카운트다운</h2>
+      <ul className="count_list">
+        {todayCountItems.map((item, index) => (
+          <CountItem key={index} countData={item} />
+        ))}
+      </ul>
+    </section>
   );
 };
 
--- src/components/TotalCount.jsx
+++ src/components/TotalCount.jsx
@@ -6,14 +6,18 @@ const TotalCount = () => {
   const modifiedCountItems = [...countItems.slice(1).reverse(), countItems[0]];
 
   return (
-    <section className="count">
-      <h2 className="blind">전체 카운트다운</h2>
-      <ul className="count_list">
-        {modifiedCountItems.map((item, index) => (
-          <CountItem key={index} countData={item} />
-        ))}
-      </ul>
-    </section>
+    <main>
+      <div className="inner">
+        <section className="count">
+          <h2 className="blind">전체 카운트다운</h2>
+          <ul className="count_list">
+            {modifiedCountItems.map((item, index) => (
+              <CountItem key={index} countData={item} />
+            ))}
+          </ul>
+        </section>
+      </div>
+    </main>
   );
 };
 
