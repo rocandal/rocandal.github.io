8c14c0a0cade387ccd2c9a66e9c5fe901e313e87
Shoupeach <shoutrawberry@gmail.com>
2024-11-17 20:32:46 +0900

    푸터 정보 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div className='inner'>
-        <p>해당 페이지는 비상업적 이용 으로 만들어 졌습니다.</p>
-        <p>기획 : 은가비 | 제작 : 은가비 슈슈 하루 | 디자인 : 하루</p>
+      <div className="inner">
+        <p>해당 페이지는 비상업적인 용도로 만들어졌습니다.</p>
+        <p>제작 : 은가비 슈슈 하루</p>
         <p>[스크린샷] 창하@카벙클 | [로고] @littleOND</p>
         <p>
           © SQUARE ENIX Published in Korea by Actoz Soft CO., LTD.
