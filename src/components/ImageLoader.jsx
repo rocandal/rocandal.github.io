c958267de3621f364d08229436e0c72b3e1ec5e1
Shoupeach <shoutrawberry@gmail.com>
2024-11-17 22:58:59 +0900

    로딩 화면 페이드 아웃 적용

--- src/components/ImageLoader.jsx
+++ src/components/ImageLoader.jsx
@@ -3,7 +3,8 @@ import imageList from '../constants/images.json';
 import { useState, useEffect } from 'react';
 
 const ImageLoader = ({ onLoadingComplete }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); // 로딩 여부
+  const [isFadingOut, setIsFadingOut] = useState(false); // 페이드아웃 여부
 
   useEffect(() => {
     let loadedCount = 0;
@@ -12,8 +13,14 @@ const ImageLoader = ({ onLoadingComplete }) => {
       loadedCount++;
 
       if (loadedCount === imageList.length) {
-        setIsLoading(false);
-        onLoadingComplete();
+        setTimeout(() => {
+          setIsFadingOut(true); // 페이드아웃 시작
+
+          setTimeout(() => {
+            setIsLoading(false); // 로딩 화면 제거
+            onLoadingComplete(); // 부모 컴포넌트에 알림
+          }, 500); // 페이드아웃 시간
+        }, 300); // 로딩 완료 후 유지 시간
       }
     };
 
@@ -25,7 +32,11 @@ const ImageLoader = ({ onLoadingComplete }) => {
     });
   }, [onLoadingComplete]);
 
-  return isLoading ? <div className="loading_screen">로딩 중...</div> : null;
+  return isLoading ? (
+    <div className={`loading_screen ${isFadingOut ? 'fade_out' : ''}`}>
+      로딩 중...
+    </div>
+  ) : null;
 };
 
 export default ImageLoader;
