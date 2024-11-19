790efa17b812982c19bbd7b292cf000f7d6a9aeb
Shoupeach <shoutrawberry@gmail.com>
2024-11-20 00:08:45 +0900

    시간 오류 수정

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,14 +1,14 @@
 const formatDate = () => {
   const now = new Date();
-  const kstOffset = 9 * 60; // UTC+9
+  const kstNow = new Date(now.getTime() + 9 * 60 * 60 * 1000);
 
-  now.setMinutes(now.getMinutes() + now.getTimezoneOffset() + kstOffset);
-
-  const formattedDate = now.toISOString().split('T')[0]; // 'YYYY-MM-DD'
+  const formattedDate = kstNow.toISOString().split('T')[0]; // 'YYYY-MM-DD'
 
   // 오늘 날짜가 2024년 11월 19일 이전인 경우
   const todayDate =
     formattedDate <= '2024-11-19' ? '2024-11-19' : formattedDate;
+
+    console.log(formattedDate)
 
   return todayDate;
 };
